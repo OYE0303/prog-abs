243c22f2baf765e5664e7b49252aad1314717b86
Eyo Chen <eyo.chen@amazingtalker.com>
2023-09-14 00:23:57 +0800

    feat: add s1/SumNumbers

---
+++ s1/SumNumbers/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "./../../Headers/printVector.h"
+#include "./../../Headers/split.h"
+#include "./../../Headers/isNumber.h"
+using namespace std;
+
+
+int main(){
+  ifstream myFile;
+  string line;
+  vector<string> lines;
+  int result = 0;
+
+  myFile.open("Section1/SumNumbers/numbers.txt");
+
+  if(!myFile.is_open()) {
+    cout << "File can't open successfully" << endl;
+    return 1;
+  }
+
+  while(myFile){
+    getline(myFile, line);
+    lines.push_back(line);
+  }
+
+  for(string line : lines){
+    vector<string> strs = split(line, " ");
+
+    for(string str : strs){
+      if(isNumber(str)) result += stoi(str);
+    }
+  }
+
+  cout << result << endl;
+
+  return 0;
+}
