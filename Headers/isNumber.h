0a5287fa5dd1b9f6b86da960eb89ddaf74e79feb
Eyo Chen <eyo.chen@amazingtalker.com>
2023-09-14 00:03:52 +0800

    feat: add isNumber

---
+++ Headers/isNumber.h
@@ -0,0 +1,9 @@
+#ifndef IsNumber
+#define IsNumber
+
+#include "./../Sources/isNumber.cpp"
+
+// Function to IsNumber the string.
+bool isNumber(string str);
+
+#endif
---
+++ Sources/isNumber.cpp
@@ -0,0 +1,16 @@
+#include <string>
+using namespace std;
+
+bool isNumber(string str){
+  if(str.length() == 0) return false;
+
+  if(str.length() > 0 && str[0] == '-') {
+    str = str.substr(1);
+  }
+
+  for(char c : str){
+    if(!isdigit(c)) return false;
+  }
+
+  return true;
+}
