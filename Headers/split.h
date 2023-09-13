b6284b5f9c3ed08412b8b07585c213c4f03b3a41
Eyo Chen <eyo.chen@amazingtalker.com>
2023-09-13 23:44:58 +0800

    feat: add custom split function

---
+++ Headers/split.h
@@ -0,0 +1,9 @@
+#ifndef Split
+#define Split
+
+#include "./../Sources/split.cpp"
+
+// Function to split the string.
+vector<string> split(string str, string delimiter);
+
+#endif
---
+++ Sources/split.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+vector<string> split(string str, string delimiter) {
+  vector<string> res;
+  int startIdx = 0;
+
+  if(delimiter.length() >= str.length()) return res;
+
+  while(startIdx < str.length()){
+    int findIdx = str.find(delimiter, startIdx);
+
+    if(findIdx == string::npos){
+      break;
+    }
+
+    string subStr = str.substr(startIdx, findIdx - startIdx);
+    res.push_back(subStr);
+    startIdx = findIdx + delimiter.length();
+  }
+
+  if(startIdx != str.length()) {
+    res.push_back(str.substr(startIdx));
+  }
+
+  return res;
+}
