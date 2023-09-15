3c9c7e3ac3624ab648b72d3911f17ae55fc0c42f
Eyo Chen <eyo.chen@amazingtalker.com>
2023-09-15 13:03:21 +0800

    feat: add pigLatin

---
+++ s1/pigLatin/main.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+
+const vector<char> vowel = {'a', 'e', 'i', 'o', 'u'};
+
+int findFirstVowel(string& str){
+  for(int i = 0; i < str.length(); i++){
+    char ch = str[i];
+    vector<const char>::iterator finder = find(vowel.begin(), vowel.end(), ch);
+
+    if(finder != vowel.end()) return i;
+  }
+
+  return -1;
+}
+
+string pigLatinReturn(string str){
+  int firstVowelIdx = findFirstVowel(str);
+
+  string firstPart = str.substr(0, firstVowelIdx);
+  string lastPart = str.substr(firstVowelIdx + 1);
+
+  return str[firstVowelIdx] + lastPart + firstPart + "ay";
+}
+
+void pigLatinReference(string& str){
+  int firstVowelIdx = findFirstVowel(str);
+
+  string firstPart = str.substr(0, firstVowelIdx);
+  string lastPart = str.substr(firstVowelIdx + 1);
+
+  str = str[firstVowelIdx] + lastPart + firstPart + "ay";
+}
+
+
+int main() {
+  string s = "julie";
+  string aa = pigLatinReturn(s);
+  
+  cout << aa << endl;
+  return 0;
+}
