c383c9b30f169a011c786d53ae0a4a75ce9ea72f
Eyo Chen <eyo.chen@amazingtalker.com>
2023-09-16 13:30:38 +0800

    feat: add crosssum

---
+++ s1/crosssum/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int crossSum(vector<vector<int>> &grid, int rowIdx, int colIdx){
+  int result = 0;
+
+  for(int c = 0; c < grid[rowIdx].size(); c++){
+    result += grid[rowIdx][c];
+  }
+
+  for(int r = 0; r < grid.size(); r++){
+    result += grid[r][colIdx];
+  }
+
+  result -= grid[rowIdx][colIdx];
+
+  return result;
+}
+
+int main() {
+  vector<vector <int>> grid =  { 
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9} 
+  };
+
+  int aa = crossSum(grid, 1, 1);
+  
+  cout << aa << endl;
+  return 0;
+}
